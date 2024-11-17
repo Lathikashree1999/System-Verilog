0c6197f308286e54c43c5d7880eb332e6089f24d
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-17 20:49:30 +0530

    Create String example - string operators .sv

---
+++ Concepts - basics/String example - string operators .sv
@@ -0,0 +1,25 @@
+module tb;
+  string firstname = "Joey";
+  string lastname  = "Tribbiani";
+
+  initial begin
+    if (firstname == lastname)
+      $display ("firstname=%s is EQUAL to lastname=%s", firstname, lastname);
+
+    if (firstname != lastname)
+      $display ("firstname=%s is NOT EQUAL to lastname=%s", firstname, lastname);
+
+    if (firstname < lastname)
+      $display ("firstname=%s is LESS THAN lastname=%s", firstname, lastname);
+
+    if (firstname > lastname)
+      $display ("firstname=%s is GREATER THAN lastname=%s", firstname, lastname);
+
+    $display ("Full Name = %s", {firstname, " ", lastname});
+
+    $display ("%s", {3{firstname}});
+
+    $display ("firstname[2]=%s lastname[2]=%s", firstname[2], lastname[2]);
+
+  end
+endmodule
