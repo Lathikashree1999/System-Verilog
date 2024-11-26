db10dd6bd42f1fc8dc893543fcfb29f41bc21175
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-26 16:48:02 +0530

    Create design .sv

---
+++ Verification Projects/DFF verified in System Verilog/design .sv
@@ -0,0 +1,12 @@
+module d_flipflop(input clk,
+                  input rst,
+                  input D,
+                  output reg Q);
+  
+  always@(posedge clk or posedge rst) begin
+    if(rst)
+      Q <= 1'b0;
+    else
+      Q <= D;
+  end
+endmodule
