96f4232adae798ee72adc0ec3214e2e33c9209e8
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-12-13 16:13:15 +0530

    Create wait fork example .sv

---
+++ Concepts - basics/SystemVerilog Processes Threads/wait fork example .sv
@@ -0,0 +1,34 @@
+module wait_fork_example;
+  initial begin
+    fork
+      begin
+        $display("Process A started at time = %0t", $time);
+        #10;
+        $display("Process A completed at time = %0t", $time);
+      end
+      begin 
+        $display("Process B started at time = %0t", $time);
+        #15;
+        $display("Process B completed at time = %0t", $time);
+      end
+      begin 
+        $display("Process C started at time = %0t", $time);
+        #20;
+        $display("Process C completed at time = %0t", $time);
+      end
+    join_any
+    
+    wait fork;
+      
+    $display("fork-join_any completed at time = %0t", $time);
+  end
+endmodule
+
+/*Output
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+Process B completed at time = 15
+Process C completed at time = 20
+fork-join_any completed at time = 20*/      
